d1f8a43db534a38ef5f65c484a1a1ccafb2c0013
Carsten König <carsten@gettingsharper.de>
2018-03-15 09:03:14 +0100

    Formeln: Num/Fractional Instanz und eval

--- Rechner/src/Formel.hs
+++ Rechner/src/Formel.hs
@@ -18,18 +18,41 @@ data Formel a
 data Operand
     = Add
     | Sub
-    | Mult
+    | Mul
     | Div
     deriving Eq
 
 
 konst :: a -> Formel a
-konst = undefined
+konst = Konstante
 
 
 eval :: Fractional a => Formel a -> a
-eval = undefined
+eval (Konstante n)  = n
+eval (BinOp op a b) =
+  let evaledOp = evalOp op
+  in eval a `evaledOp` eval b
 
+  where
+    evalOp Add = (+)
+    evalOp Sub = (-)
+    evalOp Mul = (*)
+    evalOp Div = (/)
+
+
+
+instance Num a => Num (Formel a) where
+    fromInteger n = Konstante (fromInteger n)
+    a + b    = BinOp Add a b
+    a * b    = BinOp Mul a b
+    a - b    = BinOp Sub a b
+    abs      = undefined
+    signum   = undefined
+
+
+instance Fractional a => Fractional (Formel a) where
+    a / b        = BinOp Div a b
+    fromRational = Konstante . fromRational
 
 ----------------------------------------------------------------------
 -- Formeln parsen
