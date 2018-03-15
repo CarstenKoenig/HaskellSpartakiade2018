bcc9c184ab06d0fba310832d2f4025e3b004d584
Carsten König <carsten@gettingsharper.de>
2018-03-15 09:10:21 +0100

    Formeln anzeigen (show)

--- Rechner/src/Formel.hs
+++ Rechner/src/Formel.hs
@@ -54,7 +54,15 @@ instance Fractional a => Fractional (Formel a) where
     a / b        = BinOp Div a b
     fromRational = Konstante . fromRational
 
-----------------------------------------------------------------------
+
+instance Show a => Show (Formel a) where
+  showsPrec d (Konstante n)   = showParen (d > 10) $ showsPrec 0 n
+  showsPrec d (BinOp Add a b) = showParen (d > 6)  $ showsPrec 6 a . showString " + " . showsPrec 7 b
+  showsPrec d (BinOp Sub a b) = showParen (d > 6)  $ showsPrec 6 a . showString " - " . showsPrec 7 b
+  showsPrec d (BinOp Mul a b) = showParen (d > 7)  $ showsPrec 7 a . showString " * " . showsPrec 8 b
+  showsPrec d (BinOp Div a b) = showParen (d > 7)  $ showsPrec 7 a . showString " / " . showsPrec 8 b
+
+---------------------------------------------------------------------
 -- Formeln parsen
 
 parse :: (Fractional a, Num a, Read a) => String -> Maybe (Formel a)
--- Rechner/tests/FormelSpec.hs
+++ Rechner/tests/FormelSpec.hs
@@ -18,24 +18,49 @@ main = hspec spec
 spec :: Spec
 spec = do
    describe "Beim Auswerten von Formel" $ do
-    prop "werden Konstanten auf ihren Wert berechnet" $ \ (n :: Double) ->
-      eval (konst n) `shouldApproxBe` n
-    prop "werden Additionen zweier Konstanten korrekt berechnet" $ \ (a :: Double) (b :: Double) ->
-      eval (konst a + konst b) `shouldApproxBe` (a + b)
-    prop "werden Subtraktionen zweier Konstanten korrekt berechnet" $ \ (a :: Double) (b :: Double) ->
-      eval (konst a - konst b) `shouldApproxBe` (a - b)
-    prop "werden Multiplikationen zweier Konstanten korrekt berechnet" $ \ (a :: Double) (b :: Double) ->
-      eval (konst a * konst b) `shouldApproxBe` (a * b)
-    prop "werden Divisionen zweier Konstanten korrekt berechnet" $ \ (a :: Double) (NonZero b :: NonZero Double) ->
-      eval (konst a / konst b) `shouldApproxBe` (a / b)
-    prop "ist die Subtraktion links-assoziativ" $ \ (a :: Double) (b :: Double) (c :: Double) ->
-      eval (konst a - konst b - konst c) `shouldApproxBe` ((a - b) - c)
-    prop "ist die Division links-assoziativ" $ \ (a :: Double) (NonZero b :: NonZero Double) (NonZero c :: NonZero Double) ->
-      eval (konst a / konst b / konst c) `shouldApproxBe` ((a / b) / c)
-    prop "gilt Punkt-Vor-Strich" $ \ (a :: Double) (b :: Double) (c :: Double) ->
-      eval (konst a * konst b + konst c) `shouldApproxBe` (a * b + c)
-    prop "gilt das Distributivgesetz" $ \ (a :: Double) (b :: Double) (c :: Double) ->
-      eval (konst a * (konst b + konst c)) `shouldApproxBe` (a * b + a * c)
+     prop "werden Konstanten auf ihren Wert berechnet" $ \ (n :: Double) ->
+       eval (konst n) `shouldApproxBe` n
+     prop "werden Additionen zweier Konstanten korrekt berechnet" $ \ (a :: Double) (b :: Double) ->
+       eval (konst a + konst b) `shouldApproxBe` (a + b)
+     prop "werden Subtraktionen zweier Konstanten korrekt berechnet" $ \ (a :: Double) (b :: Double) ->
+       eval (konst a - konst b) `shouldApproxBe` (a - b)
+     prop "werden Multiplikationen zweier Konstanten korrekt berechnet" $ \ (a :: Double) (b :: Double) ->
+       eval (konst a * konst b) `shouldApproxBe` (a * b)
+     prop "werden Divisionen zweier Konstanten korrekt berechnet" $ \ (a :: Double) (NonZero b :: NonZero Double) ->
+       eval (konst a / konst b) `shouldApproxBe` (a / b)
+     prop "ist die Subtraktion links-assoziativ" $ \ (a :: Double) (b :: Double) (c :: Double) ->
+       eval (konst a - konst b - konst c) `shouldApproxBe` ((a - b) - c)
+     prop "ist die Division links-assoziativ" $ \ (a :: Double) (NonZero b :: NonZero Double) (NonZero c :: NonZero Double) ->
+       eval (konst a / konst b / konst c) `shouldApproxBe` ((a / b) / c)
+     prop "gilt Punkt-Vor-Strich" $ \ (a :: Double) (b :: Double) (c :: Double) ->
+       eval (konst a * konst b + konst c) `shouldApproxBe` (a * b + c)
+     prop "gilt das Distributivgesetz" $ \ (a :: Double) (b :: Double) (c :: Double) ->
+       eval (konst a * (konst b + konst c)) `shouldApproxBe` (a * b + a * c)
+
+
+   describe "Beim `show` von Formeln" $ do
+     prop "werden Konstanten direkt ausgegeben" $ \ (n :: Double) ->
+       show (konst n) `shouldBe` show n
+     prop "werden Additionen mit + angezeigt" $ \ (a :: Double) (b :: Double) ->
+       show (konst a + konst b) `shouldBe` show a ++ " + " ++ show b
+     prop "werden Subtraktionen mit - angezeigt" $ \ (a :: Double) (b :: Double) ->
+       show (konst a - konst b) `shouldBe` show a ++ " - " ++ show b
+     prop "werden Multiplikationen mit * angezeigt" $ \ (a :: Double) (b :: Double) ->
+       show (konst a * konst b) `shouldBe` show a ++ " * " ++ show b
+     prop "werden Divisionen mit / angezeigt" $ \ (a :: Double) (b :: Double) ->
+       show (konst a / konst b) `shouldBe` show a ++ " / " ++ show b
+     prop "keine Klammern wegen Punkt vor Strich" $ \ (a :: Double) (b :: Double) (c :: Double) ->
+       show (konst a * konst b + konst c) `shouldBe` show a ++ " * " ++ show b ++ " + " ++ show c
+     prop "keine Klammern bei Mult/Add" $ \ (a :: Double) (b :: Double) (c :: Double) ->
+       show (konst a * (konst b + konst c)) `shouldBe` show a ++ " * (" ++ show b ++ " + " ++ show c ++ ")"
+     prop "Subtraktion wird links-assoziativ ausgegeben" $ \ (a :: Double) (b :: Double) (c :: Double) ->
+       show (konst a - konst b - konst c) `shouldBe` show a ++ " - " ++ show b ++ " - " ++ show c
+     prop "Subtraktion wird links-assoziativ geklammert" $ \ (a :: Double) (b :: Double) (c :: Double) ->
+       show (konst a - (konst b - konst c)) `shouldBe` show a ++ " - (" ++ show b ++ " - " ++ show c ++ ")"
+     prop "Division wird links-assoziativ ausgegeben" $ \ (a :: Double) (b :: Double) (c :: Double) ->
+       show (konst a / konst b / konst c) `shouldBe` show a ++ " / " ++ show b ++ " / " ++ show c
+     prop "Division wird links-assoziativ geklammert" $ \ (a :: Double) (b :: Double) (c :: Double) ->
+       show (konst a / (konst b / konst c)) `shouldBe` show a ++ " / (" ++ show b ++ " / " ++ show c ++ ")"
 
 
 shouldApproxBe :: Double -> Double -> Bool
